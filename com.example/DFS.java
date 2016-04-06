d67c300ea31c052456d5a5f4078321c8f81b5ba2
Pratik Shinde <pratikbhau92@gmail.com>
2016-04-06 23:43:07 +0530

    Create DFS.java

---
+++ com.example/DFS.java
@@ -0,0 +1,145 @@
+package com.kernel.graph;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+import com.kernel.graph.Graph.TYPE;
+public class DFS {
+	private static List<Graph.Vertex> V=new ArrayList<Graph.Vertex>();
+	private static List<Graph.Edge> E=new ArrayList<Graph.Edge>();
+	private static Stack<Graph.Vertex> topological_order=new Stack<Graph.Vertex>();
+	private static Graph graph1;
+	private static List<Graph.Vertex> dfsTraversal=new  ArrayList<Graph.Vertex>();
+	private static boolean visited[];
+	/**Added main method for testing purpose*/
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the graph,press -1 once done");
+		while(true)
+		{
+			int v1=sc.nextInt();
+			if(v1==-1)
+				break;
+			int v2=sc.nextInt();
+			int cost=sc.nextInt();
+			Graph.Vertex from=new Graph.Vertex(v1);
+			Graph.Vertex to=new Graph. Vertex(v2);
+			if(!V.contains(from) && !V.contains(to))
+			{
+				V.add(from);
+				V.add(to);
+				E.add(new Graph.Edge(from,to,cost));
+				
+			}
+			else if(!(V.contains(from)) && V.contains(to))
+			{
+				V.add(from);
+				E.add(new Graph.Edge(from,Graph.Vertex.getVertexAt(to.getIndex(), V),cost));
+			}
+			else if(V.contains(from) && !V.contains(to))
+			{
+				V.add(to);
+				E.add(new Graph.Edge(Graph.Vertex.getVertexAt(from.getIndex(), V),to,cost));
+			}
+			else if(V.contains(from ) && V.contains(to))
+			{
+				E.add(new Graph.Edge(Graph.Vertex.getVertexAt(from.getIndex(), V),Graph.Vertex.getVertexAt(to.getIndex(), V),cost));
+			}
+			
+		}
+		graph1=new Graph(TYPE.UNDIRECTED, V, E);
+		DepthFirstSearch(graph1);
+		sc.close();
+		for(Graph.Vertex v:dfsTraversal)
+			System.out.println(v.getIndex());
+	}
+	private  static void dfs(Graph.Vertex v1)
+	{
+		if(visited[v1.getIndex()])
+			return;
+		else
+		{
+			dfsTraversal.add(v1);
+			visited[v1.getIndex()]=true;
+		}
+		for(Graph.Edge e:v1.getEdges())
+		{
+			if(v1==e.getToVertex())
+			{
+				dfs(e.getFromVertex());
+			}
+			else
+			dfs(e.getToVertex());
+		}
+		
+		topological_order.push(v1);
+		
+	}
+	private static void dfs(Graph.Vertex v1,int parent)  throws Exception
+	{
+		if(visited[v1.getIndex()])
+			return;
+		else
+		{
+			visited[v1.getIndex()]=true;
+			for(Graph.Edge e:v1.getEdges())
+			{
+				if(v1==e.getToVertex())
+				{
+					if(visited[e.getFromVertex().getIndex()]|| e.getFromVertex().getIndex()!=parent )
+					{
+						throw new Exception("cycle detected");
+					}
+					dfs(e.getFromVertex(),v1.getIndex());
+				}
+				else
+				{
+					if(visited[e.getToVertex().getIndex()]|| e.getToVertex().getIndex()!=parent )
+					{
+						throw new Exception("cycle detected");
+					}
+					else
+						dfs(e.getToVertex(),v1.getIndex());
+				}
+			}
+		}
+	}
+	public static List<Graph.Vertex> DepthFirstSearch(Graph g)
+	{
+		visited=new boolean[g.getVertices().size()+1];
+		for(Graph.Vertex v1:g.getVertices())
+		{
+			if(!visited[v1.getIndex()])
+				dfs(v1);
+		}
+		return dfsTraversal;
+	}
+	public static Stack<Graph.Vertex> topologicalSort(Graph g)
+	{
+		visited=new boolean[g.getVertices().size()+1];
+		for(Graph.Vertex v1:g.getVertices())
+		{
+			if(!visited[v1.getIndex()])
+				dfs(v1);
+		}
+		return topological_order;
+	}
+	public static boolean isCyclic(Graph g)
+	{	
+		
+		visited=new boolean[g.getVertices().size()+1];
+		for(Graph.Vertex v1:g.getVertices())
+		{
+			if(!visited[v1.getIndex()])
+				try {
+					dfs(v1,0);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					System.out.println("Cycle detected");
+					System.exit(1);
+				}
+		}
+		return false;
+	}
+}
